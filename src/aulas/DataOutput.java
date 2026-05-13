019f150e11294142699e034da8b2ff078b298fee
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2026-05-13 17:02:39 -0300

    merge: integra repositório remoto

---
+++ src/aulas/DataOutput.java
@@ -0,0 +1,19 @@
+package aulas;
+
+public class DataOutput {
+    // Basic structure of a Java 25 code
+     void main(){
+        int number = 12;
+        int age = 25;
+        double wage = 1236.122412;
+        String name = "Nelson";
+
+        System.out.println("Hello World!");
+        System.out.println("Good Morning");
+        System.out.println(number); // displaying variable content
+        System.out.printf("%.4f%n", wage); // displaying formatted value with 0 decimal places
+        System.out.println("My name is " + name); // concatenation
+        System.out.printf("My salary is R$%.2f%n", wage);
+        System.out.printf("%s is %d years old and earns R$%.2f", name, age, wage);
+    }
+}

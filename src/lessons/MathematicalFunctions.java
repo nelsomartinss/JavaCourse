ce52c3fee6be102c1ed9a9fe6b3fc3c033188bc0
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2026-05-22 15:24:45 -0300

    Mathematical Functions

---
+++ src/lessons/MathematicalFunctions.java
@@ -0,0 +1,19 @@
+package lessons;
+
+public class MathematicalFunctions {
+    void main(){
+        double a, b, c, x, y, z;
+
+        x = 2048;
+        y = 7075;
+        z = 12454;
+
+        a = Math.sqrt(x); // Square root
+        b = Math.pow(y, 2); // Power
+        c = Math.abs(z); // Absolute value (remove the negative sign)
+
+        System.out.printf("%.0f\n", a);
+        System.out.printf("%.0f\n", b);
+        System.out.printf("%.0f\n", c);
+    }
+}

3ed614391051f214eac8123eaa6ae7ad8749cd2f
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2026-06-04 18:47:18 -0300

    loop structures exercises

---
+++ src/exercises/LoopStructureExercises/Ex004.java
@@ -0,0 +1,33 @@
+package exercises.LoopStructureExercises;
+
+import java.util.Scanner;
+
+public class Ex004 {
+    void main(){
+        Scanner sc = new Scanner(System.in);
+        double x, y;
+
+        while (true) {
+            System.out.print("Enter x: ");
+            x = sc.nextDouble();
+            System.out.print("Enter y: ");
+            y = sc.nextDouble();
+
+            if (x == 0.0 || y == 0.0){
+                break;
+            }
+
+            if (x > 0.0 && y > 0.0) {
+                System.out.println("Q1\n");
+            } else if (x < 0.0 && y > 0.0) {
+                System.out.println("Q2\n");
+            } else if (x < 0.0 && y < 0.0 ) {
+                System.out.println("Q3\n");
+            } else {
+                System.out.println("Q4\n");
+            }
+        }
+
+        sc.close();
+    }
+}
